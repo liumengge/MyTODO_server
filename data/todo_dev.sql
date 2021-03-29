24db7ef6ffe5a604447ca8ee719e678b612535be
liumengge <785965196@qq.com>
2021-03-29 09:30:24 +0800

    更新sql文件

--- data/todo_dev.sql
+++ data/todo_dev.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50553
 File Encoding         : 65001
 
-Date: 2021-03-27 17:40:30
+Date: 2021-03-27 21:39:09
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -46,8 +46,11 @@ CREATE TABLE `tasks` (
   `createdAt` datetime NOT NULL,
   `updatedAt` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Records of tasks
 -- ----------------------------
+INSERT INTO `tasks` VALUES ('36', '把项目部署到服务器上', '0', '2021-03-27 12:57:30', '2021-03-27 12:57:30');
+INSERT INTO `tasks` VALUES ('37', '完成todo项目开发', '0', '2021-03-27 13:05:33', '2021-03-27 13:05:33');
+INSERT INTO `tasks` VALUES ('38', '学习vue3', '1', '2021-03-27 13:05:43', '2021-03-27 13:05:45');
